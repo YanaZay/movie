ed93f4ccd6cbe77b1a5fe0722230218f1c28a39b
YanaZay <yanakipot@gmail.com>
2021-08-19 15:09:19 +0300

    style, html from main page 19.08.2021

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const APIURL = 'https://api.themoviedb.org/3/movie/now_playing?api_key=ebea8cfca72fdff8d2624ad7bbf78e4c';
 const IMGPATH = 'https://image.tmdb.org/t/p/w342';
+const MOVIE__CONTAINER = document.querySelector('.movie__container');
 
 async function getMovies() {
     const resp = await fetch(APIURL);
@@ -7,12 +8,14 @@ async function getMovies() {
 
     console.log(respData)
 
-    // respData.results.forEach( movie => {
-    //     const img = document.createElement('img');
-    //     img.src = IMGPATH + movie.poster_path;
-    //
-    //     document.body.appendChild(img)
-    // })
+    respData.results.forEach( movie => {
+        const movieElem = document.createElement('div');
+        movieElem.classList = 'movie';
+        movieElem.style.backgroundImage = `url(${IMGPATH + movie.poster_path})`;
+
+        MOVIE__CONTAINER.insertAdjacentElement('beforeend', movieElem)
+    })
+    console.log(respData)
 
     return respData;
 }
