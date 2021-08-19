290d573a90da3a20e665f217d8c28c7b1ee06aa9
YanaZay <yanakipot@gmail.com>
2021-08-19 13:09:04 +0300

    changes 19.08.2021

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
-const APIURL = 'http://api.themoviedb.org/3/movie/now_playing?api_key=ebea8cfca72fdff8d2624ad7bbf78e4c';
+const APIURL = 'https://api.themoviedb.org/3/movie/now_playing?api_key=ebea8cfca72fdff8d2624ad7bbf78e4c';
+const IMGPATH = 'https://image.tmdb.org/t/p/w342';
 
 async function getMovies() {
     const resp = await fetch(APIURL);
     const respData = await resp.json();
 
     console.log(respData)
+
+    // respData.results.forEach( movie => {
+    //     const img = document.createElement('img');
+    //     img.src = IMGPATH + movie.poster_path;
+    //
+    //     document.body.appendChild(img)
+    // })
+
     return respData;
 }
+getMovies()
 
