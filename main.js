fb451c814a3a2899f3a67e9496f1f16b93e9b870
Viacheslav Zaitsev <viacheslav.zajcev@gmail.com>
2021-08-25 19:48:22 +0300

    when you hover on the movie icon, a div pops up with a title and rating. get rid of errors in the console. 25.08.2021

--- main.js
+++ main.js
@@ -6,12 +6,12 @@ const MOVIES = document.querySelector('.movies');
 const MAIN_TITLE = document.querySelector('.main__title');
 const MOVIE__CONTAINER = document.querySelector('.movie__container');
 let ALL__MOVIES;
+let MOVIE__INFO = [];
+console.log(MOVIE__CONTAINER)
 
 async function getMovies() {
     const resp = await fetch(APIURL);
     const respData = await resp.json();
-
-    console.log(respData)
 
     respData.results.forEach( movie => {
         const movieElem = document.createElement('div');
@@ -20,41 +20,30 @@ async function getMovies() {
         movieElem.style.backgroundImage = `url(${IMGPATH + movie.poster_path})`;
         MOVIE__CONTAINER.insertAdjacentElement('beforeend', movieElem);
 
+        movieElem.innerHTML = `
+            <div class="movie__info">
+                <h3 class="movie__title">${movie.original_title}</h3>
+                <span class="movie__rating">${movie.vote_average}</span>
+            </div>
+        `
+        MOVIE__INFO.push(movieElem.children[0])
     })
 
     ALL__MOVIES = respData;
-    console.log(ALL__MOVIES)
+
+    console.log(MOVIE__INFO)
     return respData;
 }
 getMovies()
 
-MOVIE__CONTAINER.addEventListener('mouseover', (event) => {
-    if (!event.target.classList.contains('movie__container')) {
-        for (let i = 0; i < MOVIE__CONTAINER.children.length; i++) {
-            const currentElem = event.target;
-            currentElem.style.opacity = 0.5;
-        }
-    }
-
-})
-MOVIE__CONTAINER.addEventListener('mouseout', (event) => {
-    if (!event.target.classList.contains('movie__container')) {
-        for (let i = 0; i < MOVIE__CONTAINER.children.length; i++) {
-            const currentElem = event.target;
-            currentElem.style.opacity = 1;
-        }
-    }
-})
-
-
 MOVIES.addEventListener('click', (event) => {
-    if (!event.target.classList.contains('movie__container') && !event.target.classList.contains('main__title')) {
+    if (!event.target.classList.contains('movie__container')
+        && !event.target.classList.contains('main__title')) {
         const currentId = +event.target.id;
 
         MOVIES.style.display = 'none';
 
         const currentMovie = ALL__MOVIES.results.find(movie => movie.id === currentId);
-        console.log(currentMovie)
 
         MAIN.innerHTML = `
             <div class="current__back" style="background-image: url(${IMGPATH + currentMovie.backdrop_path})"></div>
@@ -83,13 +72,33 @@ LOGO.addEventListener('click', () => {
         movieElem.classList = 'movie';
         movieElem.id = movie.id;
         movieElem.style.backgroundImage = `url(${IMGPATH + movie.poster_path})`;
+        movieElem.innerHTML = `
+            <div class="movie__info">
+                <h3 class="movie__title">${movie.original_title}</h3>
+                <span class="movie__rating">${movie.vote_average}</span>
+            </div>
+        `
         MOVIE__CONTAINER.insertAdjacentElement('beforeend', movieElem);
         MOVIES.insertAdjacentElement('beforeend', MOVIE__CONTAINER);
         MAIN.insertAdjacentElement('beforeend', MOVIES)
         MAIN.insertAdjacentElement('afterbegin', MAIN_TITLE)
 
+
     })
 })
 
+
+MOVIE__CONTAINER.addEventListener('mouseover', (event) => {
+    if (event.target.children[0].classList.contains('movie__info')) {
+        event.target.children[0].style.display = 'flex'
+    }
+})
+
+
+MOVIE__CONTAINER.addEventListener('mouseout', (event) => {
+    if (event.target.children[0].classList.contains('movie__info')) {
+        event.target.children[0].style.display = 'none'
+    }
+})
 
 
